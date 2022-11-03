14a03baded796e66e42baed16fecc515d68f4257
ismaelneto49 <ismaelneto49@gmail.com>
2022-11-03 16:16:20 -0300

    chore: add test files

---
+++ leda/R04-01/R04-01-Rot-KLargestQuickSelectFloor-environment/src/test/orderStatistic/KLargestOrderStatisticsImplTest.java
@@ -0,0 +1,13 @@
+package orderStatistic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class KLargestOrderStatisticsImplTest {
+
+    @Test
+    void test() {
+        fail("No tests available");
+    }
+}
---
+++ leda/R04-01/R04-01-Rot-KLargestQuickSelectFloor-environment/src/test/problems/FloorBinarySearchImplTest.java
@@ -0,0 +1,13 @@
+package problems;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class FloorBinarySearchImplTest {
+
+    @Test
+    void test() {
+        fail("No tests available");
+    }
+}

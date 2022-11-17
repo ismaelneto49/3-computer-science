9df128441b8fef68b0797f2ee9df830d963bbff5
121111104 ISMAEL RAIMUNDO DA SILVA NETO <ismael.silva.neto@lcc3-086.lcc.ufcg.edu.br>
2022-11-17 11:27:41 -0300

    refactor: design refactor

--- leda/R06-01/R06-01-Rot-Linked-list-environment/src/main/java/adt/linkedList/SingleLinkedListImpl.java
+++ leda/R06-01/R06-01-Rot-Linked-list-environment/src/main/java/adt/linkedList/SingleLinkedListImpl.java
@@ -5,12 +5,13 @@ public class SingleLinkedListImpl<T> implements LinkedList<T> {
 	protected SingleLinkedListNode<T> head;
 
 	public SingleLinkedListImpl() {
-		this.head = new SingleLinkedListNode<T>();
+		SingleLinkedListNode nil = new SingleLinkedListNode<>();
+		this.head = new SingleLinkedListNode<T>(null, nil);
 	}
 
 	@Override
 	public boolean isEmpty() {
-		return this.head.next == null;
+		return this.head.next.isNIL();
 	}
 
 	@Override
@@ -20,7 +21,7 @@ public class SingleLinkedListImpl<T> implements LinkedList<T> {
 		}
 		int size = 0;
 		SingleLinkedListNode<T> temp = this.head;
-		while (temp.next != null) {
+		while (!temp.next.isNIL()) {
 			temp = temp.next;
 			size++;
 		}
@@ -33,7 +34,7 @@ public class SingleLinkedListImpl<T> implements LinkedList<T> {
 			return null;
 		}
 		SingleLinkedListNode<T> temp = this.head.next;
-		while(temp.next != null) {
+		while(!temp.next.isNIL()) {
 			if (temp.data.equals(element)) {
 				return temp.data;
 			}
@@ -45,18 +46,19 @@ public class SingleLinkedListImpl<T> implements LinkedList<T> {
 	@Override
 	public void insert(T element) {
 		SingleLinkedListNode<T> temp = this.head;
-		while(temp.next != null) {
+		while(!temp.next.isNIL()) {
 			temp = temp.next;
 		}
-		temp.next = new SingleLinkedListNode<T>(element, null);
+		SingleLinkedListNode nil = new SingleLinkedListNode<>();
+		temp.setNext(new SingleLinkedListNode<T>(element, nil));
 	}
 
 	@Override
 	public void remove(T element) {
 		SingleLinkedListNode<T> temp = this.head;
-		while(temp.next != null) {
+		while(!temp.next.isNIL()) {
 			if (temp.next.data.equals(element)) {
-				temp.next = temp.next.next;
+				temp.setNext(temp.next.next);
 				break;
 			}
 			temp = temp.next;
@@ -68,7 +70,7 @@ public class SingleLinkedListImpl<T> implements LinkedList<T> {
 		T[] array = (T[]) new Comparable[this.size()];
 		SingleLinkedListNode<T> temp = this.head;
 		int count = 0;
-		while(temp.next != null) {
+		while(!temp.next.isNIL()) {
 			array[count++] = temp.next.data;
 			temp = temp.next;
 		}

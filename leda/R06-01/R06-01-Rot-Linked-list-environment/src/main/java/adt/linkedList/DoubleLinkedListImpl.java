bffd43cd242bf9c6ab11cfc374652d15f418cd7e
ismaelneto49 <ismaelneto49@gmail.com>
2022-11-18 10:43:07 -0300

    feat: add RecursiveSingleLinkedList

--- leda/R06-01/R06-01-Rot-Linked-list-environment/src/main/java/adt/linkedList/DoubleLinkedListImpl.java
+++ leda/R06-01/R06-01-Rot-Linked-list-environment/src/main/java/adt/linkedList/DoubleLinkedListImpl.java
@@ -1,34 +1,47 @@
 package adt.linkedList;
 
-public class DoubleLinkedListImpl<T> extends SingleLinkedListImpl<T> implements
-		DoubleLinkedList<T> {
+public class DoubleLinkedListImpl<T> extends SingleLinkedListImpl<T> implements DoubleLinkedList<T> {
 
-	protected DoubleLinkedListNode<T> last;
+    protected DoubleLinkedListNode<T> last;
 
-	@Override
-	public void insertFirst(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+    @Override
+    public void insertFirst(T element) {
+        DoubleLinkedListNode<T> previous = (DoubleLinkedListNode<T>) this.head;
+        DoubleLinkedListNode<T> next = (DoubleLinkedListNode<T>) this.head.next;
+        DoubleLinkedListNode<T> node = new DoubleLinkedListNode<T>(element, next, previous);
 
-	@Override
-	public void removeFirst() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+        this.head.setNext(node);
+        if (!isEmpty()) {
+            next.setPrevious(node);
+        }
+    }
 
-	@Override
-	public void removeLast() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+    @Override
+    public void removeFirst() {
+        if (!isEmpty()) {
+            DoubleLinkedListNode<T> node = (DoubleLinkedListNode<T>) this.head.next;
+            DoubleLinkedListNode<T> previous = node.previous;
+            DoubleLinkedListNode<T> next = (DoubleLinkedListNode<T>) node.next;
 
-	public DoubleLinkedListNode<T> getLast() {
-		return last;
-	}
+            previous.next = next;
+            if (next != null) {
+                next.previous = previous;
+            }
+        }
+    }
 
-	public void setLast(DoubleLinkedListNode<T> last) {
-		this.last = last;
-	}
+    @Override
+    public void removeLast() {
+        DoubleLinkedListNode<T> nil = new DoubleLinkedListNode<T>();
+        this.last.previous.next = nil;
+    }
+
+    public DoubleLinkedListNode<T> getLast() {
+        return last;
+    }
+
+    public void setLast(DoubleLinkedListNode<T> last) {
+        this.last = last;
+    }
 
 }
--- leda/R06-01/R06-01-Rot-Linked-list-environment/src/main/java/adt/linkedList/RecursiveSingleLinkedListImpl.java
+++ leda/R06-01/R06-01-Rot-Linked-list-environment/src/main/java/adt/linkedList/RecursiveSingleLinkedListImpl.java
@@ -1,65 +1,100 @@
 package adt.linkedList;
 
+import java.util.ArrayList;
+
 public class RecursiveSingleLinkedListImpl<T> implements LinkedList<T> {
 
-	protected T data;
-	protected RecursiveSingleLinkedListImpl<T> next;
+    protected T data;
+    protected RecursiveSingleLinkedListImpl<T> next;
 
-	public RecursiveSingleLinkedListImpl() {
+    public RecursiveSingleLinkedListImpl() {
 
-	}
+    }
 
+    @Override
+    public boolean isEmpty() {
+        return this.next == null;
+    }
 
-	@Override
-	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+    @Override
+    public int size() {
+        RecursiveSingleLinkedListImpl<T> temp = this;
+        if (temp.isEmpty()) {
+            return 0;
+        }
+        temp = this.next;
+        return temp.size() + 1;
+    }
 
-	@Override
-	public int size() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+    @Override
+    public T search(T element) {
+        RecursiveSingleLinkedListImpl<T> temp = this;
+        if (temp.isEmpty()) {
+            return null;
+        }
+        if (temp.next.data.equals(element)) {
+            return temp.next.data;
+        }
+        temp = temp.next;
+        return temp.search(element);
+    }
 
-	@Override
-	public T search(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+    @Override
+    public void insert(T element) {
+        RecursiveSingleLinkedListImpl<T> node = new RecursiveSingleLinkedListImpl<>();
+        node.setData(element);
+        node.setNext(null);
 
-	@Override
-	public void insert(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+        RecursiveSingleLinkedListImpl<T> temp = this;
+        if (!temp.isEmpty()) {
+            temp = temp.next;
+            temp.insert(element);
+        } else {
+            temp.next = node;
+        }
+    }
 
-	@Override
-	public void remove(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+    @Override
+    public void remove(T element) {
+        if (!this.isEmpty()) {
+            RecursiveSingleLinkedListImpl<T> temp = this.next;
+            if (temp.data.equals(element)) {
+                this.next = temp.next.next;
+            } else {
+                temp.remove(element);
+            }
+        }
+    }
 
-	@Override
-	public T[] toArray() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+    @Override
+    public T[] toArray() {
+        T[] array = (T[]) new Comparable[this.size()];
+        RecursiveSingleLinkedListImpl<T> temp = this;
 
-	public T getData() {
-		return data;
-	}
+        if (temp.isEmpty()) {
+            return (T[]) new Comparable[0];
+        }
+        for (int i = 0; i < this.size(); i++) {
+            array[i] = temp.next.data;
+            temp = temp.next;
+        }
+        return array;
+    }
 
-	public void setData(T data) {
-		this.data = data;
-	}
+    public T getData() {
+        return data;
+    }
 
-	public RecursiveSingleLinkedListImpl<T> getNext() {
-		return next;
-	}
+    public void setData(T data) {
+        this.data = data;
+    }
 
-	public void setNext(RecursiveSingleLinkedListImpl<T> next) {
-		this.next = next;
-	}
+    public RecursiveSingleLinkedListImpl<T> getNext() {
+        return next;
+    }
+
+    public void setNext(RecursiveSingleLinkedListImpl<T> next) {
+        this.next = next;
+    }
 
 }

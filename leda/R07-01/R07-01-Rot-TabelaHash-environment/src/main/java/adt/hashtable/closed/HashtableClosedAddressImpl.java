9628a1bf82a49e6043a1b8ce8afb637824b66461
ismaelneto49 <ismaelneto49@gmail.com>
2022-11-27 13:04:44 -0300

    feat: add Hashtable closed address

--- leda/R07-01/R07-01-Rot-TabelaHash-environment/src/main/java/adt/hashtable/closed/HashtableClosedAddressImpl.java
+++ leda/R07-01/R07-01-Rot-TabelaHash-environment/src/main/java/adt/hashtable/closed/HashtableClosedAddressImpl.java
@@ -2,83 +2,122 @@ package adt.hashtable.closed;
 
 import adt.hashtable.hashfunction.HashFunction;
 import adt.hashtable.hashfunction.HashFunctionClosedAddressMethod;
+import adt.hashtable.hashfunction.HashFunctionDivisionMethod;
 import adt.hashtable.hashfunction.HashFunctionFactory;
+import adt.hashtable.open.HashtableElement;
+import util.Util;
 
-public class HashtableClosedAddressImpl<T> extends
-		AbstractHashtableClosedAddress<T> {
+import java.util.LinkedList;
+import java.util.List;
 
-	/**
-	 * A hash table with closed address works with a hash function with closed
-	 * address. Such a function can follow one of these methods: DIVISION or
-	 * MULTIPLICATION. In the DIVISION method, it is useful to change the size
-	 * of the table to an integer that is prime. This can be achieved by
-	 * producing such a prime number that is bigger and close to the desired
-	 * size.
-	 * 
-	 * For doing that, you have auxiliary methods: Util.isPrime and
-	 * getPrimeAbove as documented bellow.
-	 * 
-	 * The length of the internal table must be the immediate prime number
-	 * greater than the given size (or the given size, if it is already prime). 
-	 * For example, if size=10 then the length must
-	 * be 11. If size=20, the length must be 23. You must implement this idea in
-	 * the auxiliary method getPrimeAbove(int size) and use it.
-	 * 
-	 * @param desiredSize
-	 * @param method
-	 */
+public class HashtableClosedAddressImpl<T> extends AbstractHashtableClosedAddress<T> {
 
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	public HashtableClosedAddressImpl(int desiredSize,
-			HashFunctionClosedAddressMethod method) {
-		int realSize = desiredSize;
+    /**
+     * A hash table with closed address works with a hash function with closed
+     * address. Such a function can follow one of these methods: DIVISION or
+     * MULTIPLICATION. In the DIVISION method, it is useful to change the size
+     * of the table to an integer that is prime. This can be achieved by
+     * producing such a prime number that is bigger and close to the desired
+     * size.
+     * <p>
+     * For doing that, you have auxiliary methods: Util.isPrime and
+     * getPrimeAbove as documented bellow.
+     * <p>
+     * The length of the internal table must be the immediate prime number
+     * greater than the given size (or the given size, if it is already prime).
+     * For example, if size=10 then the length must
+     * be 11. If size=20, the length must be 23. You must implement this idea in
+     * the auxiliary method getPrimeAbove(int size) and use it.
+     *
+     * @param desiredSize
+     * @param method
+     */
 
-		if (method == HashFunctionClosedAddressMethod.DIVISION) {
-			realSize = this.getPrimeAbove(desiredSize); // real size must the
-														// the immediate prime
-														// above
-		}
-		initiateInternalTable(realSize);
-		HashFunction function = HashFunctionFactory.createHashFunction(method,
-				realSize);
-		this.hashFunction = function;
-	}
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    public HashtableClosedAddressImpl(int desiredSize, HashFunctionClosedAddressMethod method) {
+        int realSize = desiredSize;
 
-	// AUXILIARY
-	/**
-	 * It returns the prime number that is closest (and greater) to the given
-	 * number.
-	 * If the given number is prime, it is returned. 
-	 * You can use the method Util.isPrime to check if a number is
-	 * prime.
-	 */
-	int getPrimeAbove(int number) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+        if (method == HashFunctionClosedAddressMethod.DIVISION) {
+            realSize = this.getPrimeAbove(desiredSize); // real size must the
+            // the immediate prime
+            // above
+        }
+        initiateInternalTable(realSize);
+        HashFunction function = HashFunctionFactory.createHashFunction(method, realSize);
+        this.hashFunction = function;
+    }
 
-	@Override
-	public void insert(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+    // AUXILIARY
 
-	@Override
-	public void remove(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+    /**
+     * It returns the prime number that is closest (and greater) to the given
+     * number.
+     * If the given number is prime, it is returned.
+     * You can use the method Util.isPrime to check if a number is
+     * prime.
+     */
+    int getPrimeAbove(int number) {
+        while (!Util.isPrime(number)) {
+            number++;
+        }
+        return number;
+    }
 
-	@Override
-	public T search(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+    @Override
+    public void insert(T element) {
+        int keyHash = ((HashFunctionDivisionMethod) this.hashFunction).hash(element);
+        if (this.table[keyHash] == null) {
+            this.table[keyHash] = new LinkedList<>();
+        } else if (((List<HashtableElement>) this.table[keyHash]).size() > 0) {
+            this.COLLISIONS++;
+        }
+        T value = this.search(element);
+        List<HashtableElement> list = ((List<HashtableElement>) this.table[keyHash]);
+        HashtableElement node = new HashtableElement((Integer) element);
+        if (value == null) {
+            list.add(node);
+        } else {
+            for (int i = 0; i < list.size(); i++) {
+                if (list.get(i).equals(element)) {
+                    list.add(i, node);
+                }
+            }
+        }
+        this.elements++;
+    }
 
-	@Override
-	public int indexOf(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+    @Override
+    public void remove(T element) {
+        int keyHash = ((HashFunctionDivisionMethod) this.hashFunction).hash(element);
+        List<HashtableElement> list = ((List<HashtableElement>) this.table[keyHash]);
+        if (list != null) {
+            for (int i = 0; i < list.size(); i++) {
+                if (list.get(i).equals(element)) {
+                    list.remove(i);
+                    this.elements--;
+                }
+            }
+        }
+    }
+
+    @Override
+    public T search(T element) {
+        int keyHash = ((HashFunctionDivisionMethod) this.hashFunction).hash(element);
+        List<HashtableElement> list = ((List<HashtableElement>) this.table[keyHash]);
+        if (list != null) {
+            for (int i = 0; i < list.size(); i++) {
+                if (list.get(i).equals(element)) {
+                    return element;
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public int indexOf(T element) {
+        int keyHash = ((HashFunctionDivisionMethod) this.hashFunction).hash(element);
+        return this.search(element) != null ? keyHash : -1;
+    }
 
 }

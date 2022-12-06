e11566cd956104a90ad746bf7cc40f13624eaf5f
ismaelneto49 <ismaelneto49@gmail.com>
2022-12-06 09:06:57 -0300

    feat: add simple bst manipulation

--- leda/R08-01/R08-01-Rot-BST-environment/src/main/java/adt/bst/SimpleBSTManipulationImpl.java
+++ leda/R08-01/R08-01-Rot-BST-environment/src/main/java/adt/bst/SimpleBSTManipulationImpl.java
@@ -1,29 +1,65 @@
 package adt.bst;
 
+import adt.bt.BTNode;
+
 /**
- * - Esta eh a unica classe que pode ser modificada 
- * @author adalbertocajueiro
+ * - Esta eh a unica classe que pode ser modificada
  *
  * @param <T>
+ * @author adalbertocajueiro
  */
 public class SimpleBSTManipulationImpl<T extends Comparable<T>> implements SimpleBSTManipulation<T> {
 
-	@Override
-	public boolean equals(BST<T> tree1, BST<T> tree2) {
-		// TODO Implement this method
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+    @Override
+    public boolean equals(BST<T> tree1, BST<T> tree2) {
+        return this.equals((BSTNode<T>) tree1.getRoot(), (BSTNode<T>) tree2.getRoot());
+    }
 
-	@Override
-	public boolean isSimilar(BST<T> tree1, BST<T> tree2) {
-		// TODO Implement this method
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+    private boolean equals(BSTNode<T> node1, BSTNode<T> node2) {
+        if (node1.equals(node2)) {
+            if (node1.isEmpty()) {
+                return true;
+            }
+            return this.equals((BSTNode<T>) node1.getLeft(), (BSTNode<T>) node2.getLeft()) &&
+                    this.equals((BSTNode<T>) node1.getRight(), (BSTNode<T>) node2.getRight());
+        }
+        return false;
+    }
 
-	@Override
-	public T orderStatistic(BST<T> tree, int k) {
-		// TODO Implement this method
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+    @Override
+    public boolean isSimilar(BST<T> tree1, BST<T> tree2) {
+        return this.isSimilar((BSTNode<T>) tree1.getRoot(), (BSTNode<T>) tree2.getRoot());
+    }
 
+    private boolean isSimilar(BSTNode<T> node1, BSTNode<T> node2) {
+        boolean areBothNodesNIL = node1.isEmpty() && node2.isEmpty();
+        boolean isOnlyNode1NIL = node1.isEmpty() && !node2.isEmpty();
+        boolean isOnlyNode2NIL = !node1.isEmpty() && node2.isEmpty();
+        if (areBothNodesNIL) {
+            return true;
+        } else if (isOnlyNode1NIL || isOnlyNode2NIL) {
+            return false;
+        } else {
+            return this.isSimilar((BSTNode<T>) node1.getLeft(), (BSTNode<T>) node2.getLeft()) &&
+                    this.isSimilar((BSTNode<T>) node1.getRight(), (BSTNode<T>) node2.getRight());
+        }
+    }
+
+    @Override
+    public T orderStatistic(BST<T> tree, int k) {
+        if (k < 1 || k > tree.size()) {
+            return null;
+        }
+        int count = 1;
+        return this.orderStatistic(tree, tree.minimum(), count, k);
+    }
+
+    private T orderStatistic(BST<T> tree, BSTNode<T> node, int count, int k) {
+        if (count == k) {
+            return node.getData();
+        } else {
+            BSTNode<T> successor = tree.sucessor(node.getData());
+            return orderStatistic(tree, successor, ++count, k);
+        }
+    }
 }
---
+++ leda/R08-01/R08-01-Rot-BST-environment/src/test/java/adt/bst/SimpleBSTManipulationTest.java
@@ -0,0 +1,68 @@
+package adt.bst;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SimpleBSTManipulationTest {
+    private SimpleBSTManipulation<Integer> bstManipulation;
+    private BSTImpl<Integer> sameTree1;
+    private BSTImpl<Integer> sameTree2;
+    private BSTImpl<Integer> notSameTree1;
+    private BSTImpl<Integer> emptyTree;
+
+    private void fillTrees() {
+        Integer[] numbers = {6, 23, -34, 5, 9, 2, 0, 76, 12, 67, 232, -40};
+        for (int number : numbers) {
+            sameTree1.insert(number);
+            sameTree2.insert(number);
+        }
+
+        notSameTree1.insert(1);
+
+    }
+
+    @Before
+    public void setUp() {
+        this.bstManipulation = new SimpleBSTManipulationImpl<>();
+        this.sameTree1 = new BSTImpl<>();
+        this.sameTree2 = new BSTImpl<>();
+        this.notSameTree1 = new BSTImpl<>();
+        this.emptyTree = new BSTImpl<>();
+    }
+
+    @Test
+    public void testEqualsSameTrees() {
+        this.fillTrees();
+        assertTrue(this.bstManipulation.equals(this.sameTree1, this.sameTree2));
+    }
+
+    @Test
+    public void testEqualsDifferentTrees() {
+        this.fillTrees();
+        assertFalse(this.bstManipulation.equals(this.sameTree1, this.notSameTree1));
+    }
+
+    @Test
+    public void testIsSimilarSimilarTrees() {
+        this.fillTrees();
+        assertTrue(this.bstManipulation.isSimilar(this.sameTree1, this.sameTree2));
+    }
+
+    @Test
+    public void testIsSimilarNonSimilarTrees() {
+        this.fillTrees();
+        assertFalse(this.bstManipulation.isSimilar(this.sameTree1, this.notSameTree1));
+    }
+
+    @Test
+    public void testOrderStatistic() {
+        this.fillTrees();
+        Integer[] orderedNumbers = {-40, -34, 0, 2, 5, 6, 9, 12, 23, 67, 76, 232};
+        for (int i = 1; i <= orderedNumbers.length; i++) {
+            assertEquals(orderedNumbers[i-1], this.bstManipulation.orderStatistic(sameTree1, i));
+        }
+        assertNull(this.bstManipulation.orderStatistic(sameTree1, 13));
+    }
+}
